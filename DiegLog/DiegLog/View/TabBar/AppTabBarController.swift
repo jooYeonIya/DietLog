d3dc1b48a0b906bdc55cd802d2fc81289a21312c
jooYeon <jooyeonstudyios@gmail.com>
2024-04-04 13:14:00 +0900

    design: SignView 와 TabBarView의 분기점 추가 (#2)

---
+++ DiegLog/DiegLog/View/TabBar/AppTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  AppTabBarController.swift
+//  DiegLog
+//
+//  Created by Jooyeon Kang on 2024/04/04.
+//
+
+import UIKit
+
+class AppTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
