ceb6af43618e819219edd4411099a8f2a09351f5
jooYeon <jooyeonstudyios@gmail.com>
2024-04-04 12:36:36 +0900

    design: Base View 추가 (#1)

---
+++ DiegLog/DiegLog/View/Base/BaseUIViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseUIViewController.swift
+//  DiegLog
+//
+//  Created by Jooyeon Kang on 2024/04/04.
+//
+
+import UIKit
+
+class BaseUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupNavigationBar()
+        setUI()
+        setLayout()
+        setDelegate()
+    }
+    
+    func setupNavigationBar() {}
+    
+    func setUI() {}
+
+    func setLayout() {}
+    
+    func setDelegate() {}
+}
