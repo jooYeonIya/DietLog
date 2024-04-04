63e63d4a26ee1da460ec0fda873961f94915b3d5
jooYeon <jooyeonstudyios@gmail.com>
2024-04-04 12:41:46 +0900

    design: UILabel setup 추가 (#1)

---
+++ DiegLog/DiegLog/View/Base/Extension/UILabelExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UILabelExtension.swift
+//  DiegLog
+//
+//  Created by Jooyeon Kang on 2024/04/04.
+//
+
+import UIKit
+
+extension UILabel {
+    func setupLabel(text: String, font: UIFont) {
+        self.text = text
+        self.font = font
+    }
+}
