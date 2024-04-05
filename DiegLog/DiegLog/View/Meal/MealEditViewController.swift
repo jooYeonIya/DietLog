435bab9d66eea4f7751cb71b776e58c95b3264cf
jooYeon <jooyeonstudyios@gmail.com>
2024-04-05 11:35:41 +0900

    design: imagePickerView의 베이스 추가 (#9)

--- DiegLog/DiegLog/View/Meal/MealEditViewController.swift
+++ DiegLog/DiegLog/View/Meal/MealEditViewController.swift
@@ -11,6 +11,8 @@ class MealEditViewController: BaseUIViewController {
     
     private lazy var dateLabel = UILabel()
     private lazy var datePickerView = UIDatePicker()
+    private lazy var imageView = UIImageView()
+    private lazy var imageEditButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +20,12 @@ class MealEditViewController: BaseUIViewController {
     
     override func setUI() {
         setDateLabelUI()
+        setImageViewUI()
     }
     
     override func setLayout() {
         setDateLabelLayout()
+        setImageViewLayout()
     }
     
     func setDateLabelUI() {
@@ -36,6 +40,22 @@ class MealEditViewController: BaseUIViewController {
         view.addSubview(dateLabel)
     }
     
+    func setImageViewUI() {
+        
+        // 기본 이미지 찾은 뒤에 이미지와 버튼 설정 다시 해야한다
+        imageView.image = UIImage(named: "testImege")
+        imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
+        
+        imageEditButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        imageEditButton.backgroundColor = .white
+        imageEditButton.layer.cornerRadius = 20
+        imageEditButton.layer.shadowRadius = 4
+        imageEditButton.layer.shadowOpacity = 0.4
+
+        view.addSubViews([imageView, imageEditButton])
+    }
+    
     func setDateLabelLayout() {
         dateLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(24)
@@ -44,7 +64,20 @@ class MealEditViewController: BaseUIViewController {
         }
     }
     
-
+    func setImageViewLayout() {
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalTo(dateLabel)
+            make.height.equalTo(200)
+        }
+        
+        imageEditButton.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).inset(48)
+            make.trailing.equalTo(imageView)
+            make.width.height.equalTo(40)
+        }
+    }
+    
     @objc func displayDatePickerView() {
         let alert = UIAlertController(title: nil, message: "\n\n\n\n\n\n", preferredStyle: .actionSheet)
         alert.view.addSubview(datePickerView)
