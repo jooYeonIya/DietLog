e35b1a8a9b3f5490ecc476d60cc1c7c9b9958223
jooYeon <jooyeonstudyios@gmail.com>
2024-04-05 10:34:23 +0900

    design: 날짜 레이블 추가 (#9)

--- DiegLog/DiegLog/View/Meal/MealEditViewController.swift
+++ DiegLog/DiegLog/View/Meal/MealEditViewController.swift
@@ -8,8 +8,35 @@
 import UIKit
 
 class MealEditViewController: BaseUIViewController {
+    
+    private lazy var dateLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func setUI() {
+        setDateLabelUI()
+    }
+    
+    override func setLayout() {
+        setDateLabelLayout()
+    }
+    
+    func setDateLabelUI() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy년 MM월 dd일"
+        dateLabel.setupLabel(text: dateFormatter.string(from: Date()) , font: .subTitle)
+        dateLabel.textAlignment = .left
+        
+        view.addSubview(dateLabel)
+    }
+    
+    func setDateLabelLayout() {
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(24)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().inset(24)
+        }
     }
 }
