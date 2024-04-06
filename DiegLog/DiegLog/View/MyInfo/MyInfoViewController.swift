1878d536c3cd2efd540da7cc35273c4ad3ef4255
jooYeon <jooyeonstudyios@gmail.com>
2024-04-06 19:02:49 +0900

    design: nickName 추가 (#21)

--- DiegLog/DiegLog/View/MyInfo/MyInfoViewController.swift
+++ DiegLog/DiegLog/View/MyInfo/MyInfoViewController.swift
@@ -8,8 +8,33 @@
 import UIKit
 
 class MyInfoViewController: BaseUIViewController {
+    
+    private lazy var nickNameLabel = UILabel()
+    
+    let nickName = "닉네임"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func setUI() {
+        setNickNameLabelUI()
+    }
+    
+    override func setLayout() {
+        setNickNameLabelLayot()
+    }
+    
+    func setNickNameLabelUI() {
+        nickNameLabel.setupLabel(text: "안녕하세요 \(nickName)", font: .largeTitle)
+        view.addSubview(nickNameLabel)
+    }
+    
+    func setNickNameLabelLayot() {
+        nickNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(12)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().inset(24)
+        }
     }
 }
