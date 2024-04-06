019c2d45b9892d6ae8dd088e90e9a84e217d5e7f
jooYeon <jooyeonstudyios@gmail.com>
2024-04-06 19:05:30 +0900

    design: 캘린더 추가 (#21)

--- DiegLog/DiegLog/View/MyInfo/MyInfoViewController.swift
+++ DiegLog/DiegLog/View/MyInfo/MyInfoViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import FSCalendar
 
 class MyInfoViewController: BaseUIViewController {
     
     private lazy var nickNameLabel = UILabel()
+    private lazy var calendarView = FSCalendar()
     
     let nickName = "닉네임"
 
@@ -19,10 +21,16 @@ class MyInfoViewController: BaseUIViewController {
     
     override func setUI() {
         setNickNameLabelUI()
+        setCalendarViewUI()
     }
     
     override func setLayout() {
         setNickNameLabelLayot()
+        setCalendarViewLayout()
+    }
+    
+    override func setDelegate() {
+        setCalendarViewDelegate()
     }
     
     func setNickNameLabelUI() {
@@ -35,6 +43,70 @@ class MyInfoViewController: BaseUIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(12)
             make.leading.equalToSuperview().offset(24)
             make.trailing.equalToSuperview().inset(24)
+        }
+    }
+}
+
+extension MyInfoViewController: FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
+    func setCalendarViewUI() {
+        calendarView.configure()
+        view.addSubview(calendarView)
+    }
+    
+    func setCalendarViewLayout() {
+        calendarView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(nickNameLabel.snp.bottom).offset(12)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.height.equalTo(300)
+        }
+    }
+    
+    func setCalendarViewDelegate() {
+        calendarView.dataSource = self
+        calendarView.delegate = self
+    }
+    
+    // FSCalendar 내장 메소드
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarView.snp.updateConstraints {
+            $0.height.equalTo(bounds.height)
+        }
+        
+        self.view.layoutIfNeeded()
+    }
+    
+    // 날짜 선택했을 때
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+    }
+    
+    // 날짜 선택 해제했을 때
+    public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+    }
+    
+    // 토, 일 색깔 다르게 설정
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, titleDefaultColorFor date: Date) -> UIColor? {
+        let day = Calendar.current.component(.weekday, from: date) - 1
+        
+        if Calendar.current.shortWeekdaySymbols[day] == "일" {
+            return .systemRed
+        } else if Calendar.current.shortWeekdaySymbols[day] == "토" {
+            return .systemBlue
+        } else {
+            return .label
+        }
+    }
+    
+    // 오늘 날짜 밑에 글씨 추가
+    func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+
+        if dateFormatter.string(from: date) == dateFormatter.string(from: Date()) {
+            return "오늘"
+        } else {
+            return ""
         }
     }
 }
