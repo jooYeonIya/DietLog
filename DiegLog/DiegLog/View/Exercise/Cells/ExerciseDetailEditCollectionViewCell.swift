52a0043f6d6f0e637e5ff31067b4b439b3959247
jooYeon <jooyeonstudyios@gmail.com>
2024-04-06 18:18:58 +0900

    design: 컬렉션 뷰 및 카테고리 추가 버튼 (#18)

---
+++ DiegLog/DiegLog/View/Exercise/Cells/ExerciseDetailEditCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  ExerciseDetailEditCollectionViewCell.swift
+//  DiegLog
+//
+//  Created by Jooyeon Kang on 2024/04/06.
+//
+
+import UIKit
+
+class ExerciseDetailEditCollectionViewCell: UICollectionViewCell {
+    
+    override var isSelected: Bool {
+        didSet {
+            contentView.layer.borderColor = isSelected ? UIColor.blue.cgColor : UIColor.black.cgColor
+            contentView.layer.borderWidth = isSelected ? 1.5 : 1.0
+        }
+    }
+    
+    func configure(text: String) {
+        let label = UILabel()
+        label.setupLabel(text: text, font: .smallBody)
+        label.lineBreakMode = .byTruncatingTail
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        
+        contentView.layer.cornerRadius = 12
+        contentView.layer.masksToBounds = true
+        contentView.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.width.equalToSuperview().inset(12)
+            make.height.equalToSuperview()
+        }
+    }
+}
