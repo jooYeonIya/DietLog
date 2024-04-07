f1203e1466c158c8a3dee64b5a4ed8f3a3a24f47
jooYeon <jooyeonstudyios@gmail.com>
2024-04-08 00:22:51 +0900

    design: 최근 검색어 콜렉션뷰 (#23)

--- DiegLog/DiegLog/View/Search/SearchViewController.swift
+++ DiegLog/DiegLog/View/Search/SearchViewController.swift
@@ -11,23 +11,41 @@ class SearchViewController: BaseUIViewController {
     
     private lazy var searchBar = UISearchBar()
     
+    private lazy var recentSearchWordCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumInteritemSpacing = 8
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.register(RecentSearchWordCollectionViewCell.self, forCellWithReuseIdentifier: "RecentSearchWordCollectionViewCell")
+        
+        return collectionView
+    }()
+    
+    //MARK: - 변수
+    var recentSearchWords: [String] = ["그러니까", "이게", "된다고?", "그러니까 이게 된다고?"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func setUI() {
         setSearchBarUI()
+        setCollectinoViewUI()
     }
     
     override func setLayout() {
         setSearchBarLayout()
+        setCollectinoViewLayout()
     }
         
     override func setDelegate() {
         setSerachBarDelegate()
+        setCollectinoViewDelegate()
     }
 }
 
+// MARK: - SearchBar
 extension SearchViewController: UISearchBarDelegate {
     
     func setSearchBarUI() {
@@ -53,5 +71,49 @@ extension SearchViewController: UISearchBarDelegate {
     
     func setSerachBarDelegate() {
         searchBar.delegate = self
+    }
+}
+
+// MARK: - CollectionView
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func setCollectinoViewUI() {
+        view.addSubview(recentSearchWordCollectionView)
+    }
+    
+    func setCollectinoViewLayout() {
+        recentSearchWordCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(24)
+            make.leading.trailing.equalTo(searchBar)
+            make.height.equalTo(24)
+        }
+    }
+    
+    func setCollectinoViewDelegate() {
+        recentSearchWordCollectionView.delegate = self
+        recentSearchWordCollectionView.dataSource = self
+    }
+    
+    // 내장 메소드
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recentSearchWords.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecentSearchWordCollectionViewCell", for: indexPath) as? RecentSearchWordCollectionViewCell else { return UICollectionViewCell() }
+        cell.configure(with: recentSearchWords[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecentSearchWordCollectionViewCell", for: indexPath) as? RecentSearchWordCollectionViewCell else { return .zero }
+        
+        cell.recentSearchWordlabel.text = recentSearchWords[indexPath.row]
+        cell.recentSearchWordlabel.sizeToFit()
+        
+        let cellWidth = cell.recentSearchWordlabel.frame.width + 12
+        
+        return CGSize(width: cellWidth, height: 24)
     }
 }
