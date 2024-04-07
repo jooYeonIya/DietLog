11ba8064a01617fd3db75e0377dc9b6ce75d4bdc
jooYeon <jooyeonstudyios@gmail.com>
2024-04-07 23:26:08 +0900

    design: search view 추가 및 탭바 연결 (#23)

---
+++ DiegLog/DiegLog/View/Search/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  DiegLog
+//
+//  Created by Jooyeon Kang on 2024/04/07.
+//
+
+import UIKit
+
+class SearchViewController: BaseUIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
