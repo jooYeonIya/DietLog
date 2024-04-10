907fb6a4952de3c147da19b3f7c65550905a5ae6
jooyeon <>
2024-04-10 11:55:51 +0900

    feat: MyInfo CRUD 추가 (#27)

--- DiegLog/DiegLog/Model/MyInfoModel.swift
+++ DiegLog/DiegLog/Model/MyInfoModel.swift
@@ -15,3 +15,53 @@ class MyInfo: Object {
     @Persisted var muscle: Int?
     @Persisted var fat: Int?
 }
+
+extension MyInfo {
+    
+    static func addMyInfo(_ info: MyInfo) {
+        do {
+            let realm = try Realm()
+            try realm.write{
+                realm.add(info)
+            }
+        } catch {
+            print("Error func addMyInfo \(error)")
+        }
+    }
+    
+    static func getAllMyInfo() -> Results<MyInfo>? {
+        do {
+            let realm = try Realm()
+            return realm.objects(MyInfo.self)
+        } catch {
+            print("Error func getAllMyInfo \(error)")
+        }
+        
+        return nil
+    }
+
+    static func updateMyInfo(_ info: MyInfo, newInfo: MyInfo) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                info.weight = newInfo.weight
+                info.muscle = newInfo.muscle
+                info.fat = newInfo.fat
+            }
+        } catch {
+            print("Error func updateMyInfo \(error)")
+        }
+    }
+
+    static func deleteMyInfo(_ info: MyInfo) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(info)
+            }
+        } catch {
+            print("Error func deleteMyInfo \(error)")
+        }
+    }
+}
+
