fc65d8c2dedf57506208064ad2e701061c50c77c
jooYeon <jooyeonstudyios@gmail.com>
2024-04-22 07:58:43 +0900

    design: 텍스트필드 UI 변경 (#103)

--- DiegLog/DiegLog/View/Base/Extension/UITextField+extension.swift
+++ DiegLog/DiegLog/View/Base/Extension/UITextField+extension.swift
@@ -9,14 +9,16 @@ import UIKit
 
 extension UITextField {
     func setupTextField(){
-        self.layer.cornerRadius = 22
-        self.layer.masksToBounds = true
+        font = .body
         
-        self.layer.borderWidth = 1.2
-        self.layer.borderColor = UIColor.customYellow.cgColor
+        layer.cornerRadius = 22
+        layer.masksToBounds = true
+        
+        layer.borderWidth = 1.2
+        layer.borderColor = UIColor.customYellow.cgColor
         
         let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.size.height))
-        self.leftView = leftPaddingView
-        self.leftViewMode = .always
+        leftView = leftPaddingView
+        leftViewMode = .always
     }
 }
